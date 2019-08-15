6af74a84b4ee60438b25a6ca8d17595d0db1d5c3
Otto <Q1034646@ITAcademy.nzdf.mil.nz>
2019-08-16 10:21:41 +1200

    input validation started

--- NMEA/gprmc-generator.py
+++ NMEA/gprmc-generator.py
@@ -1,15 +1,15 @@
 
-from datetime import datetime
+import datetime
 import socket
 import time
 
 server_address = ('224.0.0.1',4444)
-stringname = "GPRMC"
+
 timecode = 0
 receiver_warning = "A"
 lat = 0
 lat_hem = 0
-long = 0
+longt = 0
 long_hem = 0
 sog = 0
 course = 0
@@ -17,24 +17,29 @@ date = 0
 mag = 0
 checksum = 0
 vardir = 0
+output = ''
 
 def gettime():
-    utctime = str(datetime.utcnow())
+    import datetime
+    utctime = datetime.datetime.utcnow()
+    utctime = str(utctime)
     splittime = utctime.split()
     utcstring = splittime[1].replace(':', '')
     utcsplit = utcstring.split('.')
-    utcstring = utcsplit[0]
+    utcstring = str(utcsplit[0])
     return utcstring
 
 def getdate():
-    utcdate = str(datetime.utcnow())
+    import datetime
+    utcdate = datetime.datetime.utcnow()
+    utcdate = str(utcdate)
     utcsplit = utcdate.split()
     utcdatesplit = utcsplit[0].split('-')
     year = utcdatesplit[0]
     month = utcdatesplit[1]
     day = utcdatesplit[2]
     shortyear = year[2] + year[3]
-    utcstring = day + month + shortyear
+    utcstring = str(day + month + shortyear)
     return utcstring
 
 def calculate_checksum(nmea):
@@ -47,9 +52,24 @@ def calculate_checksum(nmea):
         csum = '00' + csum
     return csum
 
+def gprmc(tc ,dte ,latt = '3650.00',lahem = 'S',lon = '17450.00',lohem = 'E',sg = '000.0',cse = '000.0',mvar = '000.0',mvard = 'E'):
+
+    global checksum 
+    nstring= ''
+    out = 'GPRMC,' + str(tc) + ',A,' + str(latt) + ',' + str(lahem) + ',' + str(lon) + ',' + str(lohem) + ',' + str(sg) + ',' + str(cse) + ',' + str(dte) + ',' + str(mvar) + ',' +str(mvard)
+    csum = calculate_checksum(out)
+    nstring = str('$' + str(out) + '*' + str(csum) + '\r\n')
+    return nstring
+
+def dtime():
+    global date
+    global timestr
+    date = str(getdate())
+    timestr = str(gettime())
+
 #lat = str(input("Enter Lat, example (4916.45): "))
 #lat_hem = str(input("N/S: "))
-#long = str(input("Enter Long, example (4916.45): "))
+#longt = str(input("Enter Long, example (4916.45): "))
 #long_hem = str(input("E/W: "))
 #mag = str(input("Enter Magnetic Variation: "))
 #sog = str(input("Enter Speed Over Ground: "))
@@ -57,25 +77,60 @@ def calculate_checksum(nmea):
 
 lat = str("3649.00")
 lat_hem = str("S")
-long = str("17450.00")
+longt = str("17450.00")
 long_hem = str("E")
 mag = str("019.0")
 sog = str("000.5")
 course = str("180.0")
 vardir = str("E")
 
+def inputs():
+    print('NMEA GPRMC Generator')
+    print('Enter Inputs...')
+    print(int(1.112))
+    
+    i_lat = ''
+    i_lat_hem = ''
+    i_longt = ''
+    i_long_hem = ''
+    i_mag = inputInt('Enter Magnetic Variation [000]:')
+    i_sog = ''
+    i_course = ''
+    i_vardir = inputChar('Input Magnetic Variation Direction [E]: ')
+    return i_lat,i_lat_hem,i_longt,i_long_hem,i_mag,i_sog,i_course,i_vardir
 
-#date = str("150819")
-#timecode = str("010820")
 
+def inputInt(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            userInput = str(userInput)
+            return userInput
+
+def inputChar(message):
+    while True:
+        userInput = input(message)
+        userInput = userInput.upper()
+        if userInput.isalpha() == True:
+            if len(userInput) == 1:
+                print(len(userInput))
+                return userInput
+            else:
+                print("Enter one character only")
+        else:
+            print("Must be a character")
+        
+        
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while 1:
-    date = str(getdate())
-    timecode = str(gettime())
-    output = stringname + ',' + timecode + ',' + receiver_warning + ',' + lat + ',' + lat_hem + ',' + long + ',' + long_hem + ',' + sog + ',' + course + ',' + date + ',' + mag + ',' + vardir
-    checksum = calculate_checksum(output)
-    nmea_string = str('$' + str(output) + '*' + str(checksum) + '\r\n')
+    lat,lat_hem,longt,long_hem,sog,course,mag,vardir = inputs()
+    dtime()
+    nmea_string = gprmc(timecode,lat,lat_hem,longt,long_hem,sog,course,date,mag,vardir)
     print (nmea_string)
     time.sleep(0.1)
     sock.sendto(nmea_string.encode(), server_address)
