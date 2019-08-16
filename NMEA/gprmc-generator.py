aa972a51f6716af3909e5097f4a29326f936c2f6
Otto <Q1034646@ITAcademy.nzdf.mil.nz>
2019-08-16 14:24:01 +1200

    input validaton complete

--- NMEA/gprmc-generator.py
+++ NMEA/gprmc-generator.py
@@ -95,53 +95,88 @@ def inputs():
     i_vardir = ''
 
 
-    # while i_lat == '':
-    #     try:
-    #         i_lat = validators.decimal(input("Input latitude [DDMM.mm]"),False,0,18000)
-    #     except:
-    #         print('Invalid Input')
-    #         time.sleep(0.5)
+    while i_lat == '':
+        try:
+            i_lat = validators.decimal(input("Input latitude [DDMM.mm]: "),False,0,18000)
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)
 
-    # while i_lat_hem == '':
-    #     try:
-    #         i_lat_hem = validators.string(input("Input Hemisphere [N/S]"),False,False,1,1)
-    #         i_lat_hem = i_lat_hem.upper()
-    #         if (not i_lat_hem == 'N') and (not i_lat_hem == 'S'):
-    #             i_lat_hem = ''
-    #             time.sleep(0.5)
-    #             print('Invalid Input')
-    #     except:
-    #         print('Invalid Input')
-    #         time.sleep(0.5)
+    while i_lat_hem == '':
+        try:
+            i_lat_hem = validators.string(input("Input Hemisphere [N/S]: "),False,False,1,1)
+            i_lat_hem = i_lat_hem.upper()
+            if (not i_lat_hem == 'N') and (not i_lat_hem == 'S'):
+                i_lat_hem = ''
+                time.sleep(0.5)
+                print('Invalid Input')
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)
 
 
-    # while i_longt == '':
-    #     try:
-    #         i_longt = validators.decimal(input("Input longitude [DDMM.mm]"),False,0,18000)
-    #     except:
-    #         print('Invalid Input')
-    #         time.sleep(0.5)
+    while i_longt == '':
+        try:
+            i_longt = validators.decimal(input("Input longitude [DDMM.mm]: "),False,0,18000)
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)
 
-    # while i_long_hem == '':
-    #     try:
-    #         i_long_hem = validators.string(input("Input Hemisphere [E/W]"),False,False,1,1)
-    #         i_long_hem = i_long_hem.upper()
-    #         if (not i_long_hem == 'E') and (not i_long_hem == 'W'):
-    #             i_long_hem = ''
-    #             time.sleep(0.5)
-    #             print('Invalid Input')
-    #     except:
-    #         print('Invalid Input')
-    #         time.sleep(0.5)
+    while i_long_hem == '':
+        try:
+            i_long_hem = validators.string(input("Input Hemisphere [E/W]: "),False,False,1,1)
+            i_long_hem = i_long_hem.upper()
+            if (not i_long_hem == 'E') and (not i_long_hem == 'W'):
+                i_long_hem = ''
+                time.sleep(0.5)
+                print('Invalid Input')
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)
+
+    while i_mag == '':
+        try:
+            i_mag = validators.decimal(input("Input Magnetic Variation [DD.dd]: "),False,0,180)
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)
+
+    while i_vardir == '':
+        try:
+            i_vardir = validators.string(input("Input Hemisphere [E/W]: "),False,False,1,1)
+            i_vardir = i_vardir.upper()
+            if (not i_vardir == 'E') and (not i_vardir == 'W'):
+                i_vardir = ''
+                time.sleep(0.5)
+                print('Invalid Input1')
+        except:
+            print('Invalid Input2')
+            time.sleep(0.5)
+
+    while i_sog == '':
+        try:
+            i_sog = validators.decimal(input("Input Speed Over Ground [kt.kt]: "),False,0,100000)
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)            
+
+    while i_course == '':
+        try:
+            i_course = validators.decimal(input("Input Course [DD.dd]: "),False,0,360)
+        except:
+            print('Invalid Input')
+            time.sleep(0.5)   
+
 
 
     return i_lat,i_lat_hem,i_longt,i_long_hem,i_mag,i_sog,i_course,i_vardir
        
         
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+lat,lat_hem,longt,long_hem,sog,course,mag,vardir = inputs()
 
 while True:
-    lat,lat_hem,longt,long_hem,sog,course,mag,vardir = inputs()
+
     dtime()
     nmea_string = gprmc(timecode,lat,lat_hem,longt,long_hem,sog,course,date,mag,vardir)
     print (nmea_string)
