f1754c60fc39d2d8b0396db21e5aea8ff7946a25
Otto <Q1034646@ITAcademy.nzdf.mil.nz>
2019-08-19 14:56:59 +1200

    sender

--- NMEA/send-NMEA.py
+++ NMEA/send-NMEA.py
@@ -1,10 +1,13 @@
 import socket
+import time
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('224.0.0.1',4444)
 
-src = input("Source File Name: ")
-
-f= open(src,"r")
-
-if f.mode == 'r':
-    contents = f.read()
+# src = input("Source File Name: ")
+while 1:
+    with open('./pos.txt','r') as f:
+        for line in f:
+            print(line)
+            sock.sendto(line.encode(), server_address)
+            time.sleep(0.2)
