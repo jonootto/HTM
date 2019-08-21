e22f5015a8f401d99cb9a96eafaf850dcf804bec
Otto <Q1034646@ITAcademy.nzdf.mil.nz>
2019-08-21 13:38:32 +1200

    linux

--- NMEA/sendgps.py
+++ NMEA/sendgps.py
@@ -4,13 +4,15 @@
 import socket
 import serial
 
-ser = serial.Serial(
-    port='COM3',\
-    baudrate=9600,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-        timeout=0)
+# ser = serial.Serial(
+#     port='COM3',\
+#     baudrate=9600,\
+#     parity=serial.PARITY_NONE,\
+#     stopbits=serial.STOPBITS_ONE,\
+#     bytesize=serial.EIGHTBITS,\
+#         timeout=0)
+
+ser = serial.Serial('/dev/ttyACM0')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('224.0.0.1',4444)
